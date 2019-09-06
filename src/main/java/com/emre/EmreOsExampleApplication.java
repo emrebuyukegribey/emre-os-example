d97b301657681c6f29462eb6ef681715348dc524
e.b.egribey@gmail.com <e.b.egribey@gmail.com>
2019-09-06 16:48:45 +0300

    added fabric8 plugin

--- src/main/java/com/emre/EmreOsExampleApplication.java
+++ src/main/java/com/emre/EmreOsExampleApplication.java
@@ -2,12 +2,20 @@ package com.emre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmreOsExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmreOsExampleApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
 	}
 
 }
